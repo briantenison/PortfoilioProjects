2fbdd21d5af81fb33719a892ea399eb41639c752
briantenison <129008782+briantenison@users.noreply.github.com>
2024-01-14 11:05:07 -0500

    Update World Food Production Analysis.sql

--- World Food Production Analysis.sql
+++ World Food Production Analysis.sql
@@ -1,4 +1,4 @@
---RECORD AMOUNT OF MAIZE EVER PRODUCED IN A YEAR
+
 Select MAX([Maize Production (tons)]) as MaximumMaizeProducedinOneYear, Entity, Year
 From ['Working Sheet$']
 WHERE Entity not like '%asia%'
@@ -11,26 +11,25 @@ AND Entity not like '%americas%'
 GROUP BY  Entity, Year
 ORDER BY MAX([Maize Production (tons)]) DESC
 
---RECORD AMOUNT OF FOOD PRODUCED IN A YEAR BY ANY COUNTRY
+
 SELECT ENTITY, YEAR, MAX([Total Food Produced (tons)]) AS TotalFoodProducedinTons
 From ['Working Sheet$']
 GROUP BY Entity, YEAR
 ORDER BY MAX([Total Food Produced (tons)]) DESC
 
 
---PUERTO RICO'S PRODUCTS IN THEIR RECORD YEAR OF 1997
+
 SELECT *
 FROM ['Working Sheet$']
 WHERE ENTITY LIKE '%PUERTO RICO%' AND YEAR LIKE '%1997%'
 
---LEAST AMOUNT OF FOOD PRODUCED IN A YEAR BY ANY COUNTRY
+
 SELECT ENTITY, YEAR, MIN([Total Food Produced (tons)]) AS TotalFoodProducedinTons
 From ['Working Sheet$']
 GROUP BY Entity, YEAR
 ORDER BY MIN([Total Food Produced (tons)])
 
 
---COUNTRIES WITH THE HIGHEST AVERAGE FOOD PRODUCTION ACROSS ALL YEARS
 SELECT ENTITY, AVG([Total Food Produced (tons)]) as AVGFoodProduced
 FROM ['Working Sheet$']
 WHERE Entity not like '%asia%'
@@ -43,7 +42,7 @@ AND Entity not like '%americas%'
 GROUP BY ENTITY
 ORDER BY AVGFoodProduced DESC
 
---CONTINENTS WITH THE HIGHEST AVERAGE FOOD PRODUCTION ACROSS ALL YEARS
+
 SELECT ENTITY, AVG([Total Food Produced (tons)]) as AVGFoodProduced
 FROM ['Working Sheet$']
 WHERE Entity like '%asia%'
